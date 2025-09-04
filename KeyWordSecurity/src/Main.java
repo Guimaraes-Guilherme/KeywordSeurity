9b3eebaa204dd9030f5ec30d357b06626af67eae
Guimaraes-Guilherme <guiguid2014@gmail.com>
2025-09-04 00:31:49 -0300

    Commit do sistema inicial com tela

--- KeyWordSecurity/src/Main.java
+++ KeyWordSecurity/src/Main.java
@@ -1,15 +1,69 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main extends Application {
+
+    Stage window;
+
+    @Override
+    public void start(Stage primaryStage) {
+        window = primaryStage;
+        window.setTitle("Password Security");
+
+        // Botões da tela inicial
+        Button gerarBtn = new Button("Gerar Senha");
+        Button guardarBtn = new Button("Guardar Senha");
+        Button visualizarBtn = new Button("Visualizar Senhas");
+
+        gerarBtn.setOnAction(e -> abrirTelaGerar());
+        guardarBtn.setOnAction(e -> abrirTelaGuardar());
+        visualizarBtn.setOnAction(e -> abrirTelaVisualizar());
+
+        VBox layout = new VBox(20);
+        layout.getChildren().addAll(gerarBtn, guardarBtn, visualizarBtn);
+
+        Scene homeScene = new Scene(layout, 400, 300);
+        window.setScene(homeScene);
+        window.show();
+    }
+
+    private void abrirTelaGerar() {
+        Button voltarBtn = new Button("Voltar");
+        voltarBtn.setOnAction(e -> start(window));
+
+        VBox layout = new VBox(20);
+        layout.getChildren().add(voltarBtn);
+
+        Scene gerarScene = new Scene(layout, 400, 300);
+        window.setScene(gerarScene);
+    }
+
+    private void abrirTelaGuardar() {
+        Button voltarBtn = new Button("Voltar");
+        voltarBtn.setOnAction(e -> start(window));
+
+        VBox layout = new VBox(20);
+        layout.getChildren().add(voltarBtn);
+
+        Scene guardarScene = new Scene(layout, 400, 300);
+        window.setScene(guardarScene);
+    }
+
+    private void abrirTelaVisualizar() {
+        Button voltarBtn = new Button("Voltar");
+        voltarBtn.setOnAction(e -> start(window));
+
+        VBox layout = new VBox(20);
+        layout.getChildren().add(voltarBtn);
+
+        Scene visualizarScene = new Scene(layout, 400, 300);
+        window.setScene(visualizarScene);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
